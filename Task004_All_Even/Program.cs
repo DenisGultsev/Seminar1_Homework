a900ca963717cb7cfead85d2b899102baef4fb80
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-06-17 20:42:31 +1000

    Task 004 All Even

---
+++ Task004_All_Even/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("{0} → ", number);
+for (int i = 2; i <= number; i += 2)
+{
+    Console.Write("{0} ", i);
+}
+
+Console.WriteLine(" ");
