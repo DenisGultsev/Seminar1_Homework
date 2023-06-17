8fff7a14448d80978f62d922bd54dec8482fec76
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-06-17 20:18:10 +1000

    Task 001 Min and Max

---
+++ Task001_Min_Max/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max, min;
+
+if (num1 > num2)
+{
+    max = num1;
+    min = num2;
+}
+else
+{
+    max = num2;
+    min = num1;
+}
+
+Console.WriteLine("max = {0}", max);
+Console.WriteLine("min = {0}", min);
