e436cf466360b28ad39e57bc6864d4977462d80f
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-06-17 20:36:36 +1000

    Task 003 Even or Odd

---
+++ Task003_Even_Odd/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine(number + " → yes");
+}
+else
+{
+    Console.WriteLine(number + " → no");
+}
